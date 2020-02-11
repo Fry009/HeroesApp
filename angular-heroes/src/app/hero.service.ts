b597411f3c499f593525806dcf0034e04c85d358
Francisco Miguel DIaz Montes <fmdiazmontes87@gmail.com>
2020-02-11 17:10:40 +0100

    heroes app version 1

---
+++ angular-heroes/src/app/hero.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { Hero } from './hero';
+import {HEROES} from './HEROES';
+import {MessageService} from './message.service';
+
+import { Observable, of } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HeroService {
+
+  constructor(private messageService: MessageService) { }
+
+  getHeroes(): Observable<Hero[]> {
+    this.messageService.add('HeroService: fetched heroes');
+    return of (HEROES);
+  }
+
+
+}
